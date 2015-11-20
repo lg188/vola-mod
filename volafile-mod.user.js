d8acaed227478726caa337c5d91e6797fa81ee2b
lg188 <lg188@tfwno.gf>
2015-11-20 17:57:12 +0100

    Implementing main config system

--- volafile-mod.user.js
+++ volafile-mod.user.js
@@ -11,15 +11,51 @@
 var vm_dcfg = {};
 
 vm_dcfg.interval = 60;
-vm_dcfg.bondary = 72;
+vm_dcfg.boundary = 72;
 vm_dcfg.debug = false;
 vm_dcfg.rate = 1;
 
 // Configuration
-function cfgSave(key,value){ localStorage.vm_cfg[key] = value;}
-function cfgLoad(key){ return localStorage.vm_cfg[key];}
 function cfgClear(key){localStorage.vm_cfg[key] = "";}
+function cfgSave(key,value){
+	if(value === vm_dcfg[key]){
+		cfgClear(key);
+	}else{
+		localStorage.vm_cfg[key] = value;
+	}
+}
 
-vm_dcfg.map(function(key){
-	console.log(key);
-});
+function cfgLoad(key){
+	if( localStorage.vm_cfg[key] !== null ){
+		if(localStorage.vm_cfg[key] == vm_dcfg[key]){ cfgClear(key);}
+		return localStorage.vm_cfg[key];
+	}else if( vm_dcfg[key] !== null){
+		return vm_dcfg[key];
+	}else{
+		return null;
+	}
+}
+
+console.log("Configuration loaded");
+
+var path = window.location.pathname;
+
+console.log("defining tick function");
+
+function tick(){
+	switch(path){
+		case "/adiscover":
+			console.log("derp page");
+		break;
+		default:
+			console.log("default page");
+
+	}
+}
+
+console.log("tick function defined");
+var derp = cfgLoad("rate");
+console.log(derp);
+setInterval( tick, cfgLoad("rate") * 10000);
+
+console.log("EOF");
