bb75d80cdc0d4b7f17c80ef35052099de48fe897
lg188 <lg188@tfwno.gf>
2016-11-23 22:08:36 +0100

    Version 0.1.1
    Added:
    	colour coding for open and disabled rooms
    Fixed:
    	localStorage IO
    TODO:
    	add a handler for closing the tab

--- volafile-mod-ng.user.js
+++ volafile-mod-ng.user.js
@@ -2,18 +2,21 @@
 // @name Volafile Mod_ng
 // @namespace org.lg188.volafile_mod_ng
 // @description Volafile moderation tools
-// @verion 0.1.0
+// @verion 0.1.1
 // @grant none
 // @author lg188
-// @require https://cdn.rawgit.com/dmauro/Keypress/master/keypress.js
 // @license GPLv2
 // ==/UserScript== 
+
+// @require https://cdn.rawgit.com/dmauro/Keypress/master/keypress.js
+// NodeList.prototype.filter = Array.prototype.filter;
+// document.querySelectorAll("td > a").filter(function(item){ return /\/r\/.*/.test(item.href); }); 
 
 try{
 	"use strict"; //jshint ignore:line
 	var vola_mod_ng = {} ;
-	function s_Load(){ vola_mod_ng = JSON.parse(localStorage.vola_mod_ng); if(vola_mod_ng.config.debug) { console.debug(vola_mod_ng);} } //jshint ignore:line
-	function s_Save(){ localStorage.vola_mod_ng = JSON.stringify(vola_mod_ng); if(vola_mod_ng.config.debug){ console.debug(vola_mod_ng);} } //jshint ignore:line
+	function s_Load(){ vola_mod_ng = JSON.parse(localStorage.vola_mod_ng);}  //jshint ignore:line
+	function s_Save(){ localStorage.vola_mod_ng = JSON.stringify(vola_mod_ng);} //jshint ignore:line
 
 	if( typeof localStorage.vola_mod_ng === 'undefined' ){
 		console.debug("no config found, creating new ");
@@ -23,38 +26,45 @@ try{
 	s_Load();
 
 
+	function doRoom(){ //jshint ignore:line
+		s_Load();
+		if(typeof vola_mod_ng.data === 'undefined'){ vola_mod_ng.data = {} ;}
+		if(typeof vola_mod_ng.data[id] === 'undefined'){ vola_mod_ng.data[id] = {};}
+		vola_mod_ng.data[id].open = true;
+		vola_mod_ng.data[id].id = id;
+		vola_mod_ng.data[id].time = new Date().getTime();
+		if(typeof this.config.disabled !== 'undefined') {vola_mod_ng.data[id].disabled = this.config.disabled;}
+		s_Save();
+	}
+
 
 	if(this.config){
 		var id = this.config.room_id;
 		// TODO: set key listener 
-		window.setInterval(() => {
-			s_Load();
-			if(typeof vola_mod_ng.data === 'undefined'){ vola_mod_ng.data = {};}
-			if(typeof vola_mod_ng.data[id] === 'undefined'){ vola_mod_ng.data['' + id] = {};}
-			vola_mod_ng.data['' + id].open = true;
-			vola_mod_ng.data['' + id].id = id;
-			vola_mod_ng.data['' + id].time = new Date().getTime();
-			s_Save();
-		}, 10000);
+		doRoom(id);
+		window.setInterval(doRoom() , 10000);
 	}else{
-		window.setInterval(() => location.reload(true)	, vola_mod_ng.config.interval);
+		//window.setInterval(() => location.reload(true)	, vola_mod_ng.config.interval);
 		s_Load();
-
-
+		var links = document.getElementsByTagName('a'); // TODO: reduce to actual room lists  TODO: remove from list once it has been edited
+		
 		for (var room in vola_mod_ng.data ){
-			if(document.getElementById("#" + room)){
-				console.log("recognised: " + room);
-			}
+		       for(var link in links){
+			       
+				if(links[link].pathname == "/r/" + room){
+					if(vola_mod_ng.data[room].disabled){
+						links[link].style.color = 'rgb(50, 50, 50)';
+
+					}else if(vola_mod_ng.data[room].open){
+						links[link].style.color = 'rgb(216, 128, 252)';
+						
+					} // TODO: else colour with treshold
+				}
+		       }
 		}
 
 	}
 	console.debug("EOF");
-
 }catch(e){
 	console.debug(e);
 }
-
-
-
-// NodeList.prototype.filter = Array.prototype.filter;
-// document.querySelectorAll("td > a").filter(function(item){ return /\/r\/.*/.test(item.href); }); 
